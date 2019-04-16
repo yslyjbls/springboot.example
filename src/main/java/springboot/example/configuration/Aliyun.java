3bcccbc1059e7aecf83eb33cee2f3ccbcd2905d3
yslyjbls <30337673+yslyjbls@users.noreply.github.com>
2019-04-16 17:46:39 +0800

    添加jsr303-方法的注解验证

---
+++ src/main/java/springboot/example/configuration/Aliyun.java
@@ -0,0 +1,73 @@
+package springboot.example.configuration;
+
+public class Aliyun {
+	
+	private String appKey;
+	
+	private String appSecret;
+	
+	private String bucket;
+	
+	private String endPoint;
+	
+	public static class Builder{
+		private String appKey;
+		
+		private String appSecret;
+		
+		private String bucket;
+		
+		private String endPoint;
+
+		public Builder setAppKey(String appKey) {
+			this.appKey = appKey;
+			return this;
+		}
+
+		public Builder setAppSecret(String appSecret) {
+			this.appSecret = appSecret;
+			return this;
+		}
+
+		public Builder setBucket(String bucket) {
+			this.bucket = bucket;
+			return this;
+		}
+
+		public Builder setEndPoint(String endPoint) {
+			this.endPoint = endPoint;
+			return this;
+		}
+		
+		public Aliyun build(){
+			return new Aliyun(this);
+		}
+	}
+	
+	public static Builder options(){
+		return new Aliyun.Builder();
+	}
+	
+	public Aliyun(Builder builder) {
+		this.appKey = builder.appKey;
+		this.appSecret = builder.appSecret;
+		this.bucket = builder.bucket;
+		this.endPoint = builder.endPoint;
+	}
+
+	public String getAppKey() {
+		return appKey;
+	}
+
+	public String getAppSecret() {
+		return appSecret;
+	}
+
+	public String getBucket() {
+		return bucket;
+	}
+
+	public String getEndPoint() {
+		return endPoint;
+	}
+}
--- src/main/java/springboot/example/controller/DemoController.java
+++ src/main/java/springboot/example/controller/DemoController.java
@@ -1,7 +1,14 @@
 package springboot.example.controller;
 
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +16,7 @@ import springboot.example.annotation.CheckParam;
 import springboot.example.annotation.NotEmpty;
 
 /**
- * demo测试
+ * demo测试(自定义注解、)
  * @author lich
  *
  */
@@ -20,11 +27,37 @@ public class DemoController {
 	@Value("${server.port}")
 	private String port;
 	
+	/**
+	 * 自定义注解控制
+	 * @param name
+	 * @return
+	 */
 	@CheckParam
 	@RequestMapping("/query")
 	public String query(@NotEmpty("不能为空") String name){
 		//int a = 1/0;
 		return name + "你好哈哈哈：" + port;
+	}
+	
+	/**
+	 * springMVC注解控制参数
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping("/paramValid4MVC")
+	public String paramValid4MVC(@Valid String name,BindingResult result){
+		validate(result);
+		//int a = 1/0;
+		return name + "你好哈哈哈：" + port;
+	}
+
+	private void validate(BindingResult result) {
+		if(result.hasErrors()){
+			List<FieldError> errorList = result.getFieldErrors();
+			for (FieldError fieldError : errorList) {
+				Assert.isTrue(false,fieldError.getDefaultMessage());
+			}
+		}
 	}
 
 }
